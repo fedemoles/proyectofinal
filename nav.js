2c21be65366661fff92d8807a72d036a54c4c82b
fedemoles <fededevcod@gmail.com>
2022-06-25 19:53:00 -0300

    nuevos cambios en proyecto final

--- nav.js
+++ nav.js
@@ -1,6 +1,3 @@
-import * as mdb from 'mdb-ui-kit'; // lib
-import { Input } from 'mdb-ui-kit'; // module
-
 document.querySelector('.first-button').addEventListener('click', function () {
 
     document.querySelector('.animated-icon1').classList.toggle('open');

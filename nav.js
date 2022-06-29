683a72c9cb25faf01804a872436fc1358d57c0a2
fedemoles <fededevcod@gmail.com>
2022-06-29 18:40:17 -0300

    cambios en nav

--- nav.js
+++ nav.js
@@ -1,5 +1,5 @@
 document
   .querySelector(".first-button")
-  .addEventListener("click", function animated() {
+  .addEventListener("click", function animateBars() {
     document.querySelector(".animated-icon1").classList.toggle("open");
   });

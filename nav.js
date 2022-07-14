688c82fc2ffc1aebf449155ed554b525a01f264f
fedemoles <fededevcod@gmail.com>
2022-07-13 23:15:33 -0300

    cambios en estilo

--- nav.js
+++ nav.js
@@ -65,3 +65,21 @@ propiedades.forEach((propiedad) => {
 fila.addEventListener('mouseleave', () => {
   propiedades.forEach(propiedad => propiedad.classList.remove('hover'));
 });
+
+/** @format */
+
+// declarando las variables del scroll top
+const scrollButton = document.querySelector(".scroll-top");
+
+if (scrollButton) {
+  window.addEventListener("scroll", () => {
+    if (scrollY > window.innerHeight * 0.0) {
+      scrollButton.style.display = "flex";
+    } else {
+      scrollButton.style.display = "none";
+    }
+  });
+  scrollButton.addEventListener("click", () => {
+    window.scrollBy(0, -2500);
+  });
+}
